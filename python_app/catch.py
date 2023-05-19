51015b51b7452142aa4fcd4bbcee776f18f8bbf7
unknown <@testadmin.genians.com>
2023-05-19 10:29:17 +0900

    catch.py modify

--- python_app/catch.py
+++ python_app/catch.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import date, timedelta
 import boto3
+from botocore.config import Config
+
 
 message = """새로운 채용 공고 확인
 """
@@ -21,23 +23,29 @@ for url in urls_title:
     
     yesterday_str = yesterday.strftime(html_selector[url[1]][3])
     pattern = re.compile(f'^{yesterday_str}')
+    hour_text = re.compile("([1-9|1[1-9]|2[0-4])시간*")
+    hate_min = re.compile("(몇|[0-9]+) ?분 ?전")
     response = requests.get(url[0], headers=header)
     soup = BeautifulSoup(response.text, "html.parser")
     origin = soup.select(html_selector[url[1]][0])
 
-    print(yesterday_str)
+    file = open("/home/user/test.txt", "w")
+    file.write(yesterday_str)
+    file.close()
 
     for body in origin:
         day = body.select_one(html_selector[url[1]][2])
         day = day.get_text()
-    
-        if re.match(pattern, day):
+        print(day)
+
+        if re.match(pattern, day) or re.match(hour_text, day) or re.match(hate_min, day):
             title = body.select_one(html_selector[url[1]][1])
             title = title.get_text()
             message += title + newline
             sub_count += 1
 
 if sub_count > 0:
-    sns = boto3.client('sns')
+    config = Config(region_name='ap-northeast-1')
+    sns = boto3.client('sns', config=config)
     response = sns.publish(TopicArn='arn:aws:sns:ap-northeast-1:127078038489:aws_cli_2', Message=message)
     print(response)
